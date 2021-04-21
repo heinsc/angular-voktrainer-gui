ce7e91f2fd70f002b859bebf7cc41e70c8a16ba4
heinsc <heinsc@t-online.de>
2021-04-21 12:29:01 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NewAccountComponent } from "./new-account/new-account.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'} 
+  , { path: 'login', component: LoginComponent}
+  , { path: 'newAccount', component: NewAccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
