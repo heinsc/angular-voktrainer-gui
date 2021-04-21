06f1868411a0db5e5650cd2bb86813884660248e
Carsten Heins <heinsc@t-online.de>
2021-04-21 12:11:06 +0200

    generate component new account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    NewAccountComponent,
   ],
   imports: [
     BrowserModule,
