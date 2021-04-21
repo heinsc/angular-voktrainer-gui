685e9d51d43bd540a6b520472c80720c4efe9bad
heinsc <heinsc@t-online.de>
2021-04-21 14:13:55 +0200

    1 file modified

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataService {
+  private REST_API_SERVER = "http://localhost:8080";
 
-  private REST_API_SERVER = "http://localhost:3000";
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  public sendGetRequest(){
-    return this.httpClient.get(this.REST_API_SERVER);
+  public sendGetRequest() {
+    return this.httpClient.get(this.REST_API_SERVER + "/appusers");
   }
 }
