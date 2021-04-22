90fbb46cdde19803d8011fcb044a4904388af339
Carsten Heins <heinsc@t-online.de>
2021-04-22 12:06:29 +0200

    initial commit angular routing example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppuserComponent } from './appuser/appuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AppuserComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule, 
-    HttpClientModule
- ],
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
