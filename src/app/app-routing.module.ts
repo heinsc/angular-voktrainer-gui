97912f22533a54632a282accca78d2a7b3eabbc4
heinsc <heinsc@t-online.de>
2021-04-22 15:59:44 +0200

    added paths to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+import { AllUsersComponent } from './all-users/all-users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+const routes: Routes = [
+  {path: 'allUsers', component: AllUsersComponent}
+  , {path: 'userDetails/:id', component: UserDetailsComponent}
+  , { path: '', redirectTo: '/allUsers', pathMatch: 'full'}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
