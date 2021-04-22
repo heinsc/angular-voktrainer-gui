d554c3cfa219c86926ccb0349eb3ee7d72b51803
heinsc <heinsc@t-online.de>
2021-04-22 15:03:00 +0200

    defined class appUser

--- src/app/app-user.ts
+++ src/app/app-user.ts
@@ -1,2 +1,21 @@
 export class AppUser {
+  id: number;
+  eMailAddress: string;
+  passwordDecoded: string;
+  lastLogin: string;
+  resultMessage: string;
+
+  constructor(
+    id: number
+    , eMailAddress: string
+    , passwordDecoded: string
+    , lastLogin: string;
+    , resultMessage: string; 
+    ) {
+      this.id = id;
+      this.eMailAddress = eMailAddress;
+      this.passwordDecoded = passwordDecoded;
+      this.lastLogin = lastLogin;
+      this.resultMessage = resultMessage;
+    }
 }
