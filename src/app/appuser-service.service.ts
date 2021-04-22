4a9188f735369db3dbbf46846ff8962db6d2fecf
heinsc <heinsc@t-online.de>
2021-04-22 09:38:08 +0200

    app user service added get method to fetch all users

--- src/app/appuser-service.service.ts
+++ src/app/appuser-service.service.ts
@@ -7,4 +7,8 @@ import { HttpClient } from '@angular/common/http';
 export class AppuserServiceService {
 
   constructor(private httpClient: HttpClient) { }
+
+  public getAllUsers() {
+    return this.httpClient.get('localhost:8080/appusers')
+  }
 }
