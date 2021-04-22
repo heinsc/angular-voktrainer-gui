9fcb89842679e0f6d5cb858137cf0e977a1046ca
heinsc <heinsc@t-online.de>
2021-04-22 13:47:13 +0200

    declared Routes and RouterModule in AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
-
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
