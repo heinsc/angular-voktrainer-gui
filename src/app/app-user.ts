ce319d2df943301fd5c2a32228bf22033c5b76f6
heinsc <heinsc@t-online.de>
2021-04-22 15:09:35 +0200

    class appUser corrections

--- src/app/app-user.ts
+++ src/app/app-user.ts
@@ -9,13 +9,13 @@ export class AppUser {
     id: number
     , eMailAddress: string
     , passwordDecoded: string
-    , lastLogin: string;
-    , resultMessage: string; 
-    ) {
-      this.id = id;
-      this.eMailAddress = eMailAddress;
-      this.passwordDecoded = passwordDecoded;
-      this.lastLogin = lastLogin;
-      this.resultMessage = resultMessage;
-    }
+    , lastLogin: string
+    , resultMessage: string
+  ) {
+    this.id = id;
+    this.eMailAddress = eMailAddress;
+    this.passwordDecoded = passwordDecoded;
+    this.lastLogin = lastLogin;
+    this.resultMessage = resultMessage;
+  }
 }
