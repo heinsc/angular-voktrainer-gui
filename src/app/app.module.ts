986b605bcb93b71df01f669a5f17b4ab18e7fb8c
Carsten Heins <heinsc@t-online.de>
2021-04-22 15:12:35 +0200

    generated component allUsers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { AllUsersComponent } from './all-users/all-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllUsersComponent
   ],
   imports: [
     BrowserModule
